c08cbe7b72f37572cfebd8c75c2d09e877a69003
tolf <140963545+tolfdog@users.noreply.github.com>
2025-09-29 10:56:05 -0700

    ugh

--- pycronometer/__init__.py
+++ pycronometer/__init__.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
-import httpx
+import requests
 import re
 import io
 import json
-import contextlib
 
 
 class Cronometer:
@@ -28,16 +27,16 @@ class Cronometer:
     GWT_USER_ID_REGEX = re.compile(r"//OK\[(?P<userid>\d+),")
 
     def __init__(self, username, password):
-        self.client = httpx.Client()
+        self.session = requests.session()
 
-        r = self.client.post(
+        r = self.session.post(
             self.API_LOGIN_URL,
             data={
                 "username": username,
                 "password": password,
                 "anticsrf": (
                     BeautifulSoup(
-                        self.client.get(self.HTML_LOGIN_URL).text, "html.parser"
+                        self.session.get(self.HTML_LOGIN_URL).text, "html.parser"
                     )
                     .find("input", {"name": "anticsrf"})
                     .get("value")
@@ -52,7 +51,7 @@ class Cronometer:
         if "error" in r:
             raise Exception(r["error"])
 
-        r = self.client.post(
+        r = self.session.post(
             self.GWT_BASE_URL,
             data=f"7|0|5|{self.GWT_RPC_MODULE_BASE}|{self.GWT_RPC_SERVICE_STRONG_NAME}|{self.GWT_RPC_SERVICE_NAME}|authenticate|java.lang.Integer/3438268394|1|2|3|4|1|5|5|-480|",
             headers=self.GWT_HTTP_HEADERS,
@@ -61,13 +60,13 @@ class Cronometer:
         if not r.text.startswith("//OK"):
             raise Exception(r.text)
 
-        if "sesnonce" in self.client.cookies:
-            self.sesnonce = self.client.cookies["sesnonce"]
+        if "sesnonce" in self.session.cookies:
+            self.sesnonce = self.session.cookies["sesnonce"]
 
         self.user_id = json.loads(r.text[4:])[0]
 
     def _generate_auth_token(self):
-        r = self.client.post(
+        r = self.session.post(
             self.GWT_BASE_URL,
             data=f"7|0|8|{self.GWT_RPC_MODULE_BASE}|{self.GWT_RPC_SERVICE_STRONG_NAME}|{self.GWT_RPC_SERVICE_NAME}|generateAuthorizationToken|java.lang.String/2004016611|I|com.cronometer.shared.user.AuthScope/2065601159|{self.sesnonce}|1|2|3|4|4|5|6|6|7|8|{self.user_id}|3600|7|2|",
             headers=self.GWT_HTTP_HEADERS,
@@ -77,13 +76,11 @@ class Cronometer:
             raise Exception(r.text)
         return json.loads(r.text[4:])[1]
 
-    @contextlib.contextmanager
     def export(self, **params):
-        with self.client.stream(
-            "GET",
+        r = self.session.get(
             "https://cronometer.com/export",
-            timeout=None,
             params={"nonce": self._generate_auth_token(), **params},
-        ) as r:
-            r.raise_for_status()
-            yield r.extensions["network_stream"]
+            stream=True,
+        )
+        r.raise_for_status()
+        return r.raw
